fb1e0ac1fe4135728a13366db5232a9ca1f017bf
TameemHisham <alkhliefattameem@gmail.com>
2026-03-26 23:57:14 +0000

    If statements, and input using scanner

--- Hello.java
+++ Hello.java
@@ -1,27 +1,15 @@
+import java.util.Scanner;
 class Hello {
     public static void main(String[] args) {
-        int num1 = 3; // 4 bytes
-        int num2 = 5;
-        int result = num1 + num2;
-        System.out.println(result);
-//        Data Types:
-//        Primitive:
-//        Integer, Float,Character,Boolean
-        double mark1 = 75.5; // 8 bytes
-        float mark2 = 12.32f ; // 4 bytes
-        long val1 = 10*100000; // 8 bytes
-        byte val2 = -127; // -127-127
-        short val3 = 3232; // 2 bytes
-        char c1 = 'a'; // 2 bytes
-        boolean isOver18 = true;
-// type casting
-        int num3 = 5;
-        double num4 = 6.4;
-        double sum = num4 + (double) num3 ;
-        System.out.println(sum);
-//        Operations
-        int counter = 0;
-        counter++; // post-increment <- fetch then increment
-        ++counter; //pre-increment <- increment and fetch
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        int age  = myObj.nextInt();
+        if (age >= 18) {
+            System.out.println("You are an adult!");
+        } else if (age > 12) {
+            System.out.println("You are a Teenager!");
+        } else {
+            System.out.println("You are a kid!");
     }
+
+}
 }
