ec648fccf7749f6a8f8c5a6973727760b2cfe004
TameemHisham <alkhliefattameem@gmail.com>
2026-03-26 23:33:37 +0000

    Primitive types

--- Hello.java
+++ Hello.java
@@ -1,5 +1,19 @@
 class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int num1 = 3; // 4 bytes
+        int num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+//        Data Types:
+//        Primitive:
+//        Integer, Float,Character,Boolean
+        double mark1 = 75.5; // 8 bytes
+        float mark2 = 12.32f ; // 4 bytes
+        long val1 = 10*100000; // 8 bytes
+        byte val2 = -127; // -127-127
+        short val3 = 3232; // 2 bytes
+        char c1 = 'a'; // 2 bytes
+        boolean isOver18 = true;
+
     }
 }
