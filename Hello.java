e80bcab5554ccf11234d9b863c92f187238369f2
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 17:27:47 +0100

    For loops

--- Hello.java
+++ Hello.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 class Hello {
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-        int age  = myObj.nextInt();
-        if (age >= 18) {
-            System.out.println("You are an adult!");
-        } else if (age > 12) {
-            System.out.println("You are a Teenager!");
-        } else {
-            System.out.println("You are a kid!");
-    }
+        for (int i = 0; i < 20; i++) {
+            if (i >= 10) {
+                System.out.print(" ".repeat(20-i));
+            } else {
+            System.out.print(" ".repeat(i));
+            }
+            System.out.println("!!HELLO!!");
+        }
 
-}
+    }
 }
