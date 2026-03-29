58e711fd298d976490b80eb0be11d1cc99eaa0a0
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 20:22:18 +0100

    String buffer methods

--- Hello.java
+++ Hello.java
@@ -2,32 +2,12 @@ import java.util.Scanner;
 
 class Hello {
     public static void main(String[] args) {
-        char[] sceretWord = {'t', 'a', 'm', 'e', 'e', 'm'};
-        int lengthOfWord = sceretWord.length;
-        int found = 0;
-        char[] guessedWord = new char[lengthOfWord];
-        Scanner myObj = new Scanner(System.in);
-        while (found != lengthOfWord) {
-            System.out.println("Enter your GUESS!!!!");
-            System.out.print("Guess: ");
-            char guessedLetter = Character.toLowerCase(myObj.next().charAt(0));
-            for (int i = 0; i < lengthOfWord; i++) {
-                if (guessedLetter == sceretWord[i]) {
-                    guessedWord[i] = guessedLetter;
-                    found += 1;
-                }
-            }
-            for (char c : guessedWord) {
-                if (c == '\0') {
-                    System.out.print("_");
-                } else {
-                    System.out.print(c);
-                }
-            }
-            System.out.println();
-            System.out.println("You found: " + found);
-        }
-        System.out.println("You won! the word was: " + new String(guessedWord));
+        StringBuffer sb = new StringBuffer("Tameem Hisham");
+        System.out.println(sb.capacity()); // length + 16
+        System.out.println(sb.length()); // length
+        sb.append(" Alkhliefat");
+        sb.delete(13, sb.length() - 1);
+        System.out.println(sb);
     }
 
 }
