452de0112f7ef2d137c54313bb33706b6540c00b
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 21:03:21 +0100

    Encapsulation and this keyword

--- Hello.java
+++ Hello.java
@@ -1,13 +1,36 @@
 import java.util.Scanner;
 
+class Human {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
 class Hello {
     public static void main(String[] args) {
-        StringBuffer sb = new StringBuffer("Tameem Hisham");
-        System.out.println(sb.capacity()); // length + 16
-        System.out.println(sb.length()); // length
-        sb.append(" Alkhliefat");
-        sb.delete(13, sb.length() - 1);
-        System.out.println(sb);
+        Human me = new Human();
+//        me.name = "tameem"
+        System.out.println(me.getAge());
+        System.out.println(me.getName());
+        me.setAge(18);
+        me.setName("Tameem");
+        System.out.println(me.getAge());
+        System.out.println(me.getName());
     }
 
 }
