452692e54636067d480459fa1dafa90488298e85
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 17:56:18 +0100

    timetable for loop... lol

--- Hello.java
+++ Hello.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 import java.util.Arrays;
 class Hello {
     public static void main(String[] args) {
-        int i;
-        for (i = 0; i < 10; i++) {
-            continue;
+        for (int i = 1; i < 13; i++) {
+            for (int j = 1; j < 13; j++) {
+                System.out.print(j*i);
+                System.out.print("   ");
+            }
+            System.out.println();
         }
-        System.out.println(i); // scope is the main method so it works!
     }
 }
