cb6608a226ea8e2ca33652c4e882646e380cd9e8
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 18:23:30 +0100

    2 classes in one file, intro to methods

--- Hello.java
+++ Hello.java
@@ -1,13 +1,15 @@
-import java.util.Scanner;
-import java.util.Arrays;
+class Calculator {
+    public int add(int num1, int num2) {
+        System.out.println("Add!");
+        return num1 + num2;
+    }
+}
+
+
 class Hello {
     public static void main(String[] args) {
-        for (int i = 1; i < 13; i++) {
-            for (int j = 1; j < 13; j++) {
-                System.out.print(j*i);
-                System.out.print("   ");
-            }
-            System.out.println();
-        }
+        Calculator mycalc = new Calculator();
+        int result = mycalc.add(4, 5);
+        System.out.println(result);
     }
 }
