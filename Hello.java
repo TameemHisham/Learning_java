4851190bae3513ea4442aa5d6d04189a2cf2010a
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 17:46:05 +0100

    Scope check 2

--- Hello.java
+++ Hello.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 import java.util.Arrays;
 class Hello {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        int i;
+        for (i = 0; i < 10; i++) {
             continue;
         }
-        System.out.println(i); // error since the scope of i in the loop
+        System.out.println(i); // scope is the main method so it works!
     }
 }
