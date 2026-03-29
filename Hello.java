0c77c8fa4a16dc8fbe254704f50d79ed3a21cecd
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 17:43:25 +0100

    Basic arrays, While loops, binary search

--- Hello.java
+++ Hello.java
@@ -1,14 +1,29 @@
 import java.util.Scanner;
+import java.util.Arrays;
 class Hello {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
-            if (i >= 10) {
-                System.out.print(" ".repeat(20-i));
-            } else {
-            System.out.print(" ".repeat(i));
+        // while loops e.g. binary search
+        int[] array = {20,12,43,2,5,9};
+        Arrays.sort(array);
+        Scanner obj = new Scanner(System.in);
+        int find = obj.nextInt();
+        boolean found = false;
+        int start = 0, end = array.length-1;
+        while (start <= end) {
+            int mid  = Math.floorDiv(end+start,2);
+            if (array[mid] == find) {
+                System.out.println("Value found at index: " + mid);
+                found = true;
+                break;
             }
-            System.out.println("!!HELLO!!");
+            if (array[mid] < find) {
+                start = mid+1;
+            } else {
+                end = mid - 1;
+            }
         }
-
+        if (!found) {
+            System.out.println("No such value exists in this array");
+        }
     }
 }
