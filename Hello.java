c96fdcfc33d9a48fb8b3ff3dcf84e81cc497ad35
TameemHisham <alkhliefattameem@gmail.com>
2026-03-29 17:45:22 +0100

    Checking scope of a loop

--- Hello.java
+++ Hello.java
@@ -2,28 +2,9 @@ import java.util.Scanner;
 import java.util.Arrays;
 class Hello {
     public static void main(String[] args) {
-        // while loops e.g. binary search
-        int[] array = {20,12,43,2,5,9};
-        Arrays.sort(array);
-        Scanner obj = new Scanner(System.in);
-        int find = obj.nextInt();
-        boolean found = false;
-        int start = 0, end = array.length-1;
-        while (start <= end) {
-            int mid  = Math.floorDiv(end+start,2);
-            if (array[mid] == find) {
-                System.out.println("Value found at index: " + mid);
-                found = true;
-                break;
-            }
-            if (array[mid] < find) {
-                start = mid+1;
-            } else {
-                end = mid - 1;
-            }
+        for (int i = 0; i < 10; i++) {
+            continue;
         }
-        if (!found) {
-            System.out.println("No such value exists in this array");
-        }
+        System.out.println(i); // error since the scope of i in the loop
     }
 }
