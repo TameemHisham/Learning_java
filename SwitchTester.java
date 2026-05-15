75795b04f7135dba21cae875a68d47e09ae5a76f
Tameem Alkhliefat <tameem.alkhliefat@student.manchester.ac.uk>
2026-05-15 07:36:12 +0000

    Step 3 lab 1 part 2

--- SwitchTester.java
+++ SwitchTester.java
@@ -3,29 +3,33 @@
 
 class SwitchTester {
  public static void main(String args[]) {
-   short a = 8;
-   short b = 3300;
-   switchExperiment(a, b);
-   System.out.println(String.format("A: %s", ((Object)a).getClass().getName()));
-   System.out.println(String.format("B: %s", ((Object)b).getClass().getName()));
+	int b = 8;
+	System.out.println("A  |  B  |  switchExperiment");
+	for (int a = 1; a < 6; a++) {
+		switchExperiment(a, b);
+	}
+
+   //System.out.println(String.format("A: %s", ((Object)a).getClass().getName()));
+   //System.out.println(String.format("B: %s", ((Object)b).getClass().getName()));
    
  }
  public static void switchExperiment(int a, int b) {
+   int val_update = b;
    switch (a) {
      case 1:
-       b -= 4;
+       val_update -= 4;
      case 2:
-       b += 4;
+       val_update += 4;
      case 3:
-       b /= 4;
+       val_update /= 4;
        break;
      case 4:
-       b *= 4;
+       val_update *= 4;
        break;
      default:
-       b = 0;
+       val_update = 0;
        break;
    }
- System.out.println("b is " + b);
+	System.out.println(String.format("%d  |  %d  |  %d", a,b,val_update));
  }
 }
