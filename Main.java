87fe1e2978223ce802042424701964cd9747d408
TameemHisham <alkhliefattameem@gmail.com>
2026-05-16 14:50:45 +0100

    clean up

--- Main.java
+++ Main.java
@@ -1,26 +1,7 @@
 
-class A {
-    public A () {
-        System.out.println("I am in A ");
-    }
-    public A (int a ) {
-        System.out.println("I am in int A ");
-    }
-}
-class B extends  A{
-    public B () {
-        System.out.println("I am in B ");
-    }
-    public B (int a ) {
-        super(3);
-        System.out.println("I am in int B ");
-    }
-}
-
 
 
 class Main {
     public static void main(String[] args) {
-        B b = new B(1);
      }
 }
