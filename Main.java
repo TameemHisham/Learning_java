9eaf3cb9783b896723c98cb30db444e6ce709398
TameemHisham <alkhliefattameem@gmail.com>
2026-05-16 14:19:37 +0100

    idk

--- Main.java
+++ Main.java
@@ -16,38 +16,5 @@ class SimpleCalculator {
 }
 class Main {
     public static void main(String[] args) {
-        SimpleCalculator calc = new SimpleCalculator();
-
-        if (args.length > 0) {
-            String input = args[0];
-
-            String currentNumber = "";
-            int result = 0;
-            char operation = '+';
-
-            for (int i = 0; i < input.length(); i++) {
-                char c = input.charAt(i);
-
-                if (Character.isDigit(c)) {
-                    currentNumber += c;
-                }
-
-                if (!Character.isDigit(c) || i == input.length() - 1) {
-                    int num = Integer.parseInt(currentNumber);
-
-                    switch (operation) {
-                        case '+': result = calc.add(result, num); break;
-                        case '-': result = calc.sub(result, num); break;
-                        case '*': result = calc.mul(result, num); break;
-                        case '/': result = (int) calc.div(result, num); break;
-                    }
-
-                    operation = c;
-                    currentNumber = "";
-                }
-            }
-
-            System.out.println(result);
-        }
     }
 }
