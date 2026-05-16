9137d30f49f8629fcc8b9ac2337d72c86d20b4d3
Tameem Alkhliefat <tameem.alkhliefat@student.manchester.ac.uk>
2026-05-16 12:24:23 +0000

    Creating the minimal gunctionality

--- HangmanDriver.java
+++ HangmanDriver.java
@@ -6,9 +6,20 @@ class HangmanDriver {
 	public static void main(String[] args) {
 		Console my_console = System.console();
 		Hangman my_hangman = new Hangman("");
+		HangmanDictionary my_dictionary = new HangmanDictionary();
 		String in;
 		in = my_console.readLine("Enter your name: ");
 		my_hangman.setPlayerName(in);
 		System.out.println("Welcome " + my_hangman.getPlayerName()+"!");
+		String guess_word = my_dictionary.getWord();
+		my_hangman.setWord(guess_word);
+		my_hangman.tryChar('e');
+		my_hangman.tryChar('l');
+		my_hangman.tryChar('s');
+		my_hangman.tryChar('a');
+		
+		
+		System.out.println(my_hangman.getCurrentState());
+		
 	}
 }
