fc07950431f902570086bc38305e02cbad4d86f3
TameemHisham <alkhliefattameem@gmail.com>
2026-05-16 14:35:28 +0100

    super and default constructors

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
 
+class A {
+    public A () {
+        System.out.println("I am in A ");
+    }
+    public A (int a ) {
+        System.out.println("I am in int A ");
+    }
+}
+class B extends  A{
+    public B () {
+        System.out.println("I am in B ");
+    }
+    public B (int a ) {
+        super(3);
+        System.out.println("I am in int B ");
+    }
+}
+
+
+
 class Main {
     public static void main(String[] args) {
-        AdvancedCalculator calc = new AdvancedCalculator();
-        System.out.println(calc.pow(2,4));
-        System.out.println(calc.add(5,6));
-        System.out.println(calc.mul(2,6));
-        System.out.println(calc.log(10));
-    }
+        B b = new B(1);
+     }
 }
