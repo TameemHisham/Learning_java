2bad60e5a3f62e571185e9b81e50250c43ea179d
TameemHisham <alkhliefattameem@gmail.com>
2026-05-21 11:08:33 +0100

    array lists, and hashet

--- Main.java
+++ Main.java
@@ -1,48 +1,25 @@
-
-enum Gender {
-    MALE,
-    FEMALE
-}
-
-interface Human {
-    String bestHuman = "TAMEEM";
-    String getName();
-    int getAge();
-    Gender getGender();
-}
-
-class Student implements Human {
-    private  String name;
-    private int age;
-    private int year;
-    private Gender gender;
-    private  double grade;
-
-    public Student(String tameem, int i, Gender gender, int i1, double v) {
-        this.name =name;
-        this.age =age;
-        this.year =year;
-        this.gender =gender;
-        this.grade =grade;
-    }
-
-
-    public String getName() {
-        return this.name;
-    }
-
-    public int getAge() {
-        return this.age;
-    }
-
-    public Gender getGender() {
-        return this.gender;
-    }
-}
-
+import java.util.*;
 class Main {
     public static void main(String[] args) {
-        Student tameem = new Student("tameem", 18, Gender.MALE, 1, 83.0);
-        System.out.println(Human.bestHuman);
+        List<String> list = new ArrayList<String>();
+        Set<Integer> set = new HashSet<Integer>();
+        list.add("TAMEEM");
+        list.add("MANAF");
+        list.add("SHAHIAH");
+        list.add("YASMEEN");
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+        set.add(1);
+        set.add(3);
+        System.out.println(set.contains(4));
+        System.out.println(set.size());
+        System.out.println(set);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.indexOf("MANAF"));
+        System.out.println(list.get(3));
     }
 }
