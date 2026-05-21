4e4ce848d7d1e55e287841068afbe7faeafaf918
TameemHisham <alkhliefattameem@gmail.com>
2026-05-21 11:13:36 +0100

    Maps

--- Main.java
+++ Main.java
@@ -1,25 +1,14 @@
 import java.util.*;
 class Main {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
-        Set<Integer> set = new HashSet<Integer>();
-        list.add("TAMEEM");
-        list.add("MANAF");
-        list.add("SHAHIAH");
-        list.add("YASMEEN");
-        set.add(1);
-        set.add(2);
-        set.add(3);
-        set.add(4);
-        set.add(5);
-        set.add(1);
-        set.add(3);
-        System.out.println(set.contains(4));
-        System.out.println(set.size());
-        System.out.println(set);
-        System.out.println(list);
-        System.out.println(list.size());
-        System.out.println(list.indexOf("MANAF"));
-        System.out.println(list.get(3));
+        Map<String, Integer> grades = new HashMap<>();
+        Scanner my_scanner = new Scanner(System.in);
+        System.out.print(">>> ");
+        String name = my_scanner.nextLine();
+        System.out.println();
+        grades.put("Tameem", 87);
+        grades.put("Hisham", 97);
+        grades.put("Yasmeen", 80);
+        System.out.println(grades.get(name));
     }
 }
