06be63d545188373b83c249523f0b9d4ee669d4d
TameemHisham <alkhliefattameem@gmail.com>
2026-05-21 12:32:05 +0100

    Comparator

--- Main.java
+++ Main.java
@@ -1,14 +1,38 @@
 import java.util.*;
+
+
+class SortCodes implements Comparator {
+    public int compare (Object i, Object j) {
+        String i_string = (String)i;
+        String j_string = (String)j;
+        if (i_string.compareTo(j_string) > 0) {
+            return 1;
+        } else if (i_string.compareTo(j_string) < 0){
+            return -1;
+        } else {
+            int value1 = Integer.parseInt(i_string);
+            int value2 = Integer.parseInt(j_string);
+            if (value1 > value2) {
+                return 1;
+            } else if (value1 < value2) return 1;
+
+        }
+        return 0;
+    }
+}
+
+
 class Main {
     public static void main(String[] args) {
-        Map<String, Integer> grades = new HashMap<>();
-        Scanner my_scanner = new Scanner(System.in);
-        System.out.print(">>> ");
-        String name = my_scanner.nextLine();
-        System.out.println();
-        grades.put("Tameem", 87);
-        grades.put("Hisham", 97);
-        grades.put("Yasmeen", 80);
-        System.out.println(grades.get(name));
+        List<String> codes = new ArrayList<String>();
+        codes.add("E05");
+        codes.add("A13");
+        codes.add("A12");
+        codes.add("B13");
+        System.out.println(codes);
+        Comparator my_comparator  = new SortCodes();
+        codes.sort(my_comparator);
+        System.out.println(codes);
+
     }
 }
